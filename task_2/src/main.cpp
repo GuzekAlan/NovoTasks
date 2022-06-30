08209a55e93bc552a1f26883ebd38a77c474e195
Alan Guzek <alanguzek@gmail.com>
2022-06-30 09:14:46 +0200

    color changes on points

--- task_2/src/main.cpp
+++ task_2/src/main.cpp
@@ -2,13 +2,16 @@
 #include <SFML/Graphics.hpp>
 #include <cmath>
 #include <iostream>
+#include <vector>
 
-auto getCenterRotationMatrix(float angle, float dx, float dy) -> sf::Transform {
+auto getCenterRotationMatrix(float angle, const sf::RectangleShape& rectangle) -> sf::Transform {
+    auto dx = rectangle.getSize().x/2 + rectangle.getPosition().x;
+    auto dy = rectangle.getSize().y/2 + rectangle.getPosition().y;
     return sf::Transform().translate(dx, dy).rotate(angle).translate(-dx, -dy); // Rotating in center of a rectangle
 }
 
 auto getVectorLength(const sf::Vector2f& vector) -> double {
-    return fabs(pow(vector.x, 2) + pow(vector.y, 2));
+    return sqrt(pow(vector.x, 2) + pow(vector.y, 2));
 }
 
 auto getCircleCenter(const sf::CircleShape& circle) -> sf::Vector2f {
@@ -19,19 +22,20 @@ auto isPointInCircle(const sf::Vector2f& point, const sf::CircleShape& circle) -
     return getVectorLength(point- getCircleCenter(circle)) <= circle.getRadius();
 }
 
-auto intersectCircleRect(const sf::RectangleShape& rectangle, const sf::CircleShape& circle) -> bool {
-    auto point = rectangle.getPosition();
-    if(isPointInCircle(point, circle))
+auto intersectCircleRect(const sf::RectangleShape& rectangle, const sf::CircleShape& circle, int angle) -> bool {
+    auto Points = std::vector<sf::Vector2f>{rectangle.getPosition(),
+                                            rectangle.getPosition(),
+                                            rectangle.getPosition(),
+                                            rectangle.getPosition()};
+    Points[1].x += rectangle.getSize().x;;
+    Points[2].x += rectangle.getSize().x;
+    Points[2].y += rectangle.getSize().y;
+    Points[3].y += rectangle.getSize().y;
+    if(std::any_of(Points.begin(), Points.end(), [&circle, &angle, &rectangle](const sf::Vector2f& point){
+            return isPointInCircle(getCenterRotationMatrix(angle, rectangle).transformPoint(point), circle);
+    })){
         return true;
-    point.x += rectangle.getSize().x;
-    if(isPointInCircle(point, circle))
-        return true;
-    point.y += rectangle.getSize().y;
-    if(isPointInCircle(point, circle))
-        return true;
-    point.x -= rectangle.getSize().x;
-    if(isPointInCircle(point, circle))
-        return true;
+    }
 
     // More things to check
 
@@ -126,7 +130,7 @@ int main(){
 
         if(clock.getElapsedTime().asSeconds() >= rotate_speed ){
             rotate_angle = (rotate_angle + delta_rotate)%360;
-            if(intersectCircleRect(rectangle, circle)){
+            if(intersectCircleRect(rectangle, circle, rotate_angle)){
                 std::cout << "Intersecting" << std::endl;
                 rectangle.setFillColor(color_r_good);
                 circle.setFillColor(color_c_good);
@@ -141,8 +145,7 @@ int main(){
 
 
         window.draw(circle);
-        window.draw(rectangle, getCenterRotationMatrix(rotate_angle,
-                                                       rect_size.x/2+rect_pos.x, rect_size.y/2+rect_pos.y));
+        window.draw(rectangle, getCenterRotationMatrix(rotate_angle, rectangle));
         window.display();
     }
 
